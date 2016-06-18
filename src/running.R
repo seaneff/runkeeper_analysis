1f0fae225cd6db67daf8f967038246d145ccd1b2
Steph Eaneff <steph.eaneff@gmail.com>
2016-06-17 23:00:35 -0400

    still needs some work, segmenting by both distance and speed relative to distance

--- src/running.R
+++ src/running.R
@@ -2,7 +2,7 @@
 ## set working directory and load libraries #########################
 #####################################################################
 
-setwd("~/Documents/website_private/running")
+setwd("~/Documents/workspace/runkeeper_analysis")
 library(tm)
 library(slam)
 library(ggplot2)
@@ -41,7 +41,7 @@ makeFootnote <- function(text = format(Sys.time(), "%d %b %Y"),
 ## load and format runkeeper data ###################################
 #####################################################################
 
-workout <- read.csv("runkeeper-data-export-41471231-2016-06-12-2322/cardioActivities.csv")
+workout <- read.csv("data/runkeeper-data-export-41471231-2016-06-12-2322/cardioActivities.csv")
 workout$Notes <- as.character(workout$Notes)
 workout$notes <- as.POSIXct(workout$Date)
 ## time range is set manually in export, specify which one was used here
@@ -58,7 +58,7 @@ run$time_of_day <- as.POSIXct(run$Average.Pace, format = "%:H:%M:%S")
 ## How long are my runs? ############################################
 #####################################################################
 
-pdf("run_length.pdf", height = 4, width = 6)
+pdf("results/run_length.pdf", height = 5, width = 6)
 par(mar = mar_default + c(2, 0, 0, 0))
 hist(run$Distance..mi,
      ylab = "Number of Runs",
@@ -82,7 +82,7 @@ dev.off()
 ## Does my running speed differ by run length? ######################
 #####################################################################
 
-pdf("pace_vs_distance.pdf", height = 4, width = 6)
+pdf("results/pace_vs_distance.pdf", height = 4, width = 6)
 par(mar = mar_default + c(2, 2, 0, 0))
 plot(run$Distance..mi, run$avg_pace,
      xlab = "Miles per Run",
@@ -92,23 +92,42 @@ plot(run$Distance..mi, run$avg_pace,
 makeFootnote(paste("Based on RunKeeper Run Data\nDate Range:", time_range))
 dev.off()
 
+#############################################################################
+## basic regression: pace ~ run length  (dont' consider date of run) ########
+#############################################################################
+
+## for future plots, this will help to define runs that may be 
+## faster than expected given the distance vs. slower than expected
+## given the distance
+
+pace_by_time <- lm(run$Average.Speed..mph. ~ run$Distance..mi)
+
+## for runs between 1/1/2016 and 6/12/2016, there is no strong evidence
+## than my face differed based on climb (elevation gain)
+
 ################################################
 ## process free text ###########################
 ################################################
 
 run_text <- Corpus(VectorSource(run$Notes))
 ## remote SMART stopwords: http://jmlr.csail.mit.edu/papers/volume5/lewis04a/a11-smart-stop-list/english.stop
+
 run_text_processed <- tm_map(run_text, removeWords, stopwords("SMART"))
+
 run_text_processed <- tm_map(run_text_processed, removePunctuation)
 
 ## generate document term matrix from processed text data
-dt <- as.matrix(DocumentTermMatrix(run_text_processed))
+dt_initial <- as.matrix(DocumentTermMatrix(run_text_processed))
+
+## there is a bug in removeWords that causes stopwords
+## not to be removed if they're the first word in a piece of text
+dt <- dt_initial[,-which(colnames(dt_initial) %in% stopwords("SMART"))]
 
 ################################################
 ## basic word cloud ############################
 ################################################
 
-pdf("note_wordcloud.pdf", height = 5, width = 6)
+pdf("results/note_wordcloud.pdf", height = 5, width = 6)
 set.seed(1203)
 wordcloud(colnames(dt),
           col_sums(dt),
@@ -146,7 +165,7 @@ plotting_limit <- 1.1*max(c(abs(min(length_level$x)),
                     abs(max(length_level$x)))) 
 
 
-pdf("positioned_wordcloud.pdf", height = 5, width = 9)
+pdf("results/positioned_wordcloud_distance.pdf", height = 5, width = 9)
 ggplot(length_level, aes(x = x , y = y)) + 
   geom_text(aes(size = total_count,
                 label = row.names(length_level),
@@ -165,3 +184,65 @@ ggplot(length_level, aes(x = x , y = y)) +
   theme_bw()
 dev.off()
 
+################################################
+## distance and speed word cloud ###############
+################################################
+
+## use the residuals of the very simple fitted model (fit above) of pace ~ distance
+## to see if a run is faster than expected for its distance or slower than expected
+## for its distance
+slow_counts <- as.vector(rollup(dt[which(pace_by_time$resid < 0),], 1, FUN = function(x)sum(ifelse(x > 0, 1, 0))))
+fast_counts <- as.vector(rollup(dt[which(pace_by_time$resid >= 0),], 1, FUN = function(x)sum(ifelse(x > 0, 1, 0))))
+names(slow_counts) <- colnames(dt); names(fast_counts) <- colnames(dt)
+
+pace_level <- cbind.data.frame(fast = fast_counts, slow = slow_counts)
+
+pace_level$total_count <- apply(pace_level, 1, sum)
+pace_level$p_fast <- pace_level$fast / sum(pace_by_time$resid >= 0)
+pace_level$p_slow <- pace_level$slow / sum(pace_by_time$resid < 0)
+## add 0.001 to avoid dividing by zero or taking the log of 0
+pace_level$rr <- (pace_level$p_fast + 0.001) / (pace_level$p_slow + 0.001)
+pace_level$log_rr <- log((pace_level$p_fast + 0.001) / (pace_level$p_slow + 0.001)) 
+
+## add a bit of a jitter (add random noise along the x axis to avoid overlapping words)
+plotting_randomness_factor <- 1.5
+set.seed(1202)
+length_level$x <- length_level$log_rr + rnorm(mean = 0, sd = plotting_randomness_factor, n = nrow(length_level))
+set.seed(0311)
+length_level$y <- pace_level$log_rr + rnorm(mean = 0, sd = plotting_randomness_factor, n = nrow(pace_level))
+
+length_level$alpha <- (length_level$total_count - min(length_level$total_count))/
+                       max(length_level$total_count)
+
+length_level$alpha[which(length_level$alpha == 0)] <- 0.01
+
+plotting_limit_x <- 1.1*max(c(abs(min(length_level$x)),
+                            abs(max(length_level$x)))) 
+
+plotting_limit_y <- 1.1*max(c(abs(min(length_level$y)),
+                              abs(length_level$y))) 
+
+
+pdf("results/positioned_wordcloud_distance_pace.pdf", height = 5, width = 9)
+ggplot(length_level, aes(x = x , y = y)) + 
+  geom_text(aes(size = total_count,
+                label = row.names(length_level),
+                colour = log_rr),
+            alpha = 0.6) + 
+  scale_size(range = c(3, 5), name = "Number of Runs\nwith Notes\nMentioning Word") +
+  scale_color_gradient(low = "#8E0045", high = "#0A4877", guide = "none") +  
+  scale_x_continuous(breaks = c(-1*plotting_limit_x, 0, plotting_limit_x),
+                     limits = c(-1.2*plotting_limit_x, 1.2*plotting_limit_x),
+                     labels = c(paste("Said More after\nShort Runs\n(runs less than", round(median_distance, 1), "miles)"),
+                                "Said Equally after\nShort and Long Runs",
+                                paste("Said More after\nLong Runs\n(runs more than",  round(median_distance, 1), "miles)"))) +
+  scale_y_continuous(breaks = c(-1*plotting_limit_y, 0, plotting_limit_y),
+                     limits = c(-1.2*plotting_limit_y, 1.2*plotting_limit_y),
+                     labels = c("Said More after\nSlower Runs of Similar Distances",
+                                "Said Equally after\nFaster and Slower Runs",
+                                "Said More after\nFaster Runs of Similar Distances")) +
+  ggtitle("Relaxing vs. Strugglefest:\nWord use After Shorter vs. Longer Runkeeper Runs") +
+  xlab("") +
+  ylab("") +
+  theme_bw()
+dev.off()
